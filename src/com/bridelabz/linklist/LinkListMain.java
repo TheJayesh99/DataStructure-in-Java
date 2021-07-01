902c5d4e67a66ae386189bd47a39ff724cd19722
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-01 17:29:15 +0530

    Added : Method to delete first element

--- src/com/bridelabz/linklist/LinkListMain.java
+++ src/com/bridelabz/linklist/LinkListMain.java
@@ -5,9 +5,10 @@ public class LinkListMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to LinkList");
 		LinkListService linkList = new LinkListService();
-		linkList.append(56);
+		linkList.add(56);
 		linkList.append(70);
 		linkList.insertAfter(56,30);
+		linkList.pop();
 		linkList.print();
 	}
 
