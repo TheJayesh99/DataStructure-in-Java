3c1cde67efad9b7078f12cc7d474c35896a22efb
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-01 13:14:21 +0530

    Added : Method to append nodes

--- src/com/bridelabz/linklist/LinkListMain.java
+++ src/com/bridelabz/linklist/LinkListMain.java
@@ -5,9 +5,9 @@ public class LinkListMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to LinkList");
 		LinkListService linkList = new LinkListService();
-		linkList.add(70);
-		linkList.add(30);
-		linkList.add(56);
+		linkList.append(56);
+		linkList.append(30);
+		linkList.append(70);
 		linkList.print();
 	}
 
--- src/com/bridelabz/linklist/LinkListService.java
+++ src/com/bridelabz/linklist/LinkListService.java
@@ -1,5 +1,6 @@
 package com.bridelabz.linklist;
 
+
 public class LinkListService
 {
 
@@ -12,9 +13,31 @@ public class LinkListService
 		newNode.next = head;
 		this.head = newNode;
 	}
+
+	public void append(int data)
+	{
+		LinkList newNode = new LinkList();
+		newNode.data = data;
+		newNode.next = null;
+		if (head == null) 
+		{			
+			this.head = newNode;
+		}
+		else
+		{
+			LinkList currentNode = head;
+			while (currentNode.next != null)
+			{
+				currentNode = currentNode.next;
+			}
+			currentNode.next = newNode;
+		}
+	}
+	
 	public void print()
 	{
 		LinkList tempNode = head;
+		System.out.println("The required linklist is");
 		while(tempNode.next != null)
 		{
 			System.out.print(tempNode.data+" -> ");
