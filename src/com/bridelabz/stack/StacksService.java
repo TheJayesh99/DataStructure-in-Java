e6b7c893f9c866d5c43c02570ec4ef0a2411b475
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-02 13:56:56 +0530

    Added : Method to pop and peak on stack

--- src/com/bridelabz/stack/StacksService.java
+++ src/com/bridelabz/stack/StacksService.java
@@ -2,14 +2,27 @@ package com.bridelabz.stack;
 
 import com.bridelabz.linklist.LinkListService;
 
-public class StacksService<T> {
-	
+public class StacksService<T>
+{
+
 	LinkListService<T> linkedList= new LinkListService<T>();
-	public void push(T data) {
+	public void push(T data) 
+	{
 		linkedList.add(data);
 	}
-	public void print() {
+	public void print()
+	{
 		linkedList.print();
+	}
+	public void peak()
+	{
+		System.out.println("The data at the top of stack is "+ linkedList.head.data);
+
+	}
+	public void pop()
+	{
+		linkedList.pop();
+
 	}
 
 }
