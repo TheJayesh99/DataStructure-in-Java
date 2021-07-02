486bc7ea2aaab271570e0aa58d6d057fd8b04c33
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-02 17:03:43 +0530

    Modifed : solve conflict

--- src/com/bridelabz/queue/QueueMain.java
+++ src/com/bridelabz/queue/QueueMain.java
@@ -5,7 +5,6 @@ public class QueueMain
 	public static void main(String[] args) 
 	{
 		QueueService<Integer> queue = new QueueService<Integer>();
-		
 		queue.enqueue(56);
 		queue.enqueue(30);
 		queue.enqueue(70);
