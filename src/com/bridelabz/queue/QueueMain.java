2e78ab798cf5b59aa4cc099a6e29a936d8a5b691
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-02 16:50:35 +0530

    Added : change input values

--- src/com/bridelabz/queue/QueueMain.java
+++ src/com/bridelabz/queue/QueueMain.java
@@ -5,6 +5,7 @@ public class QueueMain
 	public static void main(String[] args) 
 	{
 		QueueService<Integer> queue = new QueueService<Integer>();
+		
 		queue.enqueue(56);
 		queue.enqueue(30);
 		queue.enqueue(70);
